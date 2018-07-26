4a64814729e2d920092e99a704a8aad9330474aa
苏金林 <sujl@greenmarks.org>
2018-07-26 10:22:55 +0800

    7.0适配

---
+++ app/src/main/java/com/jinlin/mvpdemo/util/custom/Test.java
@@ -0,0 +1,10 @@
+package com.jinlin.mvpdemo.util.custom;
+
+/*********************************************************
+ * Test
+ * @author jinlin
+ * @email jinlinsu@qq.com
+ * @dete 2018/7/26
+ *********************************************************/
+public class Test {
+}
